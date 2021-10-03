db0632e739c2d377e75514d25c96afac9390e9b5
AHMAD HASSAN <70092917+ahmadhassan361@users.noreply.github.com>
2021-10-04 02:53:11 +0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 from django.core.management.utils import get_random_secret_key
 from urllib.parse import urlparse
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -149,7 +150,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/workspace/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
